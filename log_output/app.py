8f808d79bf1261e2b55534e5903dcbb8889b0c7b
Kassudin <kasper.kinnarinen@live.fi>
2026-07-15 14:34:22 +0300

    1.7

--- log_output/app.py
+++ log_output/app.py
@@ -1,10 +1,27 @@
-import time
+import os
 import uuid
 from datetime import datetime, timezone
 
+from flask import Flask
+
+app = Flask(__name__)
+
 random_string = str(uuid.uuid4())
 
-while True:
-    timestamp = datetime.now(timezone.utc).isoformat(timespec="milliseconds").replace("+00:00", "Z")
-    print(f"{timestamp}: {random_string}", flush=True)
-    time.sleep(5)
+def current_timestamp():
+    return (
+    datetime.now(timezone.utc)
+    .isoformat()
+    .replace('+00:00', 'Z')
+    )
+
+@app.route('/')
+def index():
+    timestamp = current_timestamp()
+    output = f"{timestamp} - {random_string}"
+
+    return output
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 3000))
+    app.run(host="0.0.0.0", port=port)
