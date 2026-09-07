06c1014329f54a0e9e60bc45c9a93c23f9ebdfd7
Kassudin <kasper.kinnarinen@live.fi>
2026-09-07 09:35:06 +0300

    2.7

--- logping/ping-pong/app.py
+++ logping/ping-pong/app.py
@@ -1,23 +1,65 @@
 import os
-
+import psycopg2
 from flask import Flask
 
 app = Flask(__name__)
 
-counter = 0
+database_url = os.environ["DATABASE_URL"]
+
+def init_db():
+    connection = psycopg2.connect(database_url)
+    cursor = connection.cursor()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS pingpong (
+    id INTEGER PRIMARY KEY,
+    counter INTEGER NOT NULL
+    )
+    """)
+    cursor.execute("""
+    Insert INTO pingpong (id, counter)
+    VALUES (1, 0) 
+    ON CONFLICT (id) DO NOTHING
+    """)
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+def get_counter():
+    connection = psycopg2.connect(database_url)
+    cursor = connection.cursor()
+    cursor.execute("""
+    SELECT counter
+    FROM pingpong
+    WHERE id = 1
+    """)
+    counter = cursor.fetchone()[0]
+    cursor.close()
+    connection.close()
+    return counter
 
 
 @app.route("/pingpong")
 def pingpong():
-    global counter
-    response = f"pong: {counter}"
-    counter += 1
-    return response
+    connection = psycopg2.connect(database_url)
+    cursor = connection.cursor()
+    cursor.execute("""
+    UPDATE pingpong
+    SET counter = counter + 1
+    WHERE id = 1
+    RETURNING counter - 1
+    """)
+    counter = cursor.fetchone()[0]
+    connection.commit()
+    cursor.close()
+    connection.close()
+    return f"pong: {counter}"
 
 @app.route("/pings")
 def pings():
-    return str(counter)
+    return str(get_counter())
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 3000))
+    init_db()
+    port = int(os.environ["PORT"])
     app.run(host="0.0.0.0", port=port)
+    
